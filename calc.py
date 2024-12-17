6f194ccace0d067c6f1143acd950912f75a6b9f5
annewak <wakomoanne@gmail.com>
2024-12-17 13:46:37 +0000

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,10 @@
+number1 = float(input("Enter your first number choice: "))
+number2 = float(input("Enter your second number choice: "))
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+quotient_result = number1 / number2
+print("Sum = " + str(sum_result))
+print("Difference = " + str(difference_result))
+print("Product = " + str(product_result))
+print("Quotient = " + str(quotient_result))
